55ba7e76758706fe81df0bd053103a943caedb4c
RubenOlander <rubenolander@gmail.com>
2024-10-10 14:08:48 +0200

    escape

--- src/app/howto/page.tsx
+++ src/app/howto/page.tsx
@@ -8,7 +8,7 @@ export default function HowToPage() {
       <h2 className="text-2xl">About this tool</h2>
       <p>
         Welcome to my family calendar creation tool. This was first thought up
-        when me and my wife couldn't settle on a calendar at the shop. Either
+        when me and my wife couldn´t settle on a calendar at the shop. Either
         the images were tacky, or the number of columns were wrong. So I decided
         to create my own.
       </p>
@@ -20,11 +20,11 @@ export default function HowToPage() {
       <p>
         The next step is to select the number of columns you will need, from one
         to five. After that you input the names of the column owners; either
-        your own name or perhaps categories. There's an 18 character limit.
+        your own name or perhaps categories. There´s an 18 character limit.
       </p>
       <p>
         Lastly, click Create PDF to download a generated file for you to print!
-        As of now it's designed for a standing A4 paper with margins taken into
+        As of now it´s designed for a standing A4 paper with margins taken into
         account as well as space on top for you to draw a nice picture.
       </p>
     </main>
